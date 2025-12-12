b20322d1a550dc619f360c29e593d0b5e4b7c074
rosarioborgesi <borgesiros@gmail.com>
2025-12-12 17:30:13 +0100

    Completed test testBridgeAllTokens

--- test/CrossChainTest.t.sol
+++ test/CrossChainTest.t.sol
@@ -208,5 +208,16 @@ contract CrossChainTest is Test {
             sepoliaToken,
             arbSepoliaToken
         );
+        vm.selectFork(arbSepoliaFork);
+        vm.warp(block.timestamp + 20 minutes);
+        bridgeTokens(
+            arbSepoliaToken.balanceOf(user),
+            arbSepoliaFork,
+            sepoliaFork,
+            arbSepoliaNetworkDetails,
+            sepoliaNetworkDetails,
+            arbSepoliaToken,
+            sepoliaToken
+        );
     }
 }
